7651c9d350cd5f8ef9394d1b33d1e5ba6daba002
Senanur Sarıkaya <144035209+senaskaya@users.noreply.github.com>
2026-07-02 22:13:56 +0300

    Add files via upload

---
+++ odev1.sql
@@ -0,0 +1,10 @@
+SELECT title,description FROM film;
+
+SELECT * FROM film WHERE length > 60 AND length < 75;
+
+SELECT * FROM film WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR 28.99);
+
+SELECT first_name, last_name FROM customer WHERE first_name = 'Mary';
+
+SELECT * FROM film WHERE length <= 50 AND (rental_rate != 2.99 OR rental_rate != 4.99);
+
